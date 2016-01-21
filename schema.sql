7127d91fe723c6f44b6d26d4b49953779bf210d1
Dylan Porteus <dylanporteus@aol.com>
2016-01-20 20:31:33 -0500

    add signup functionality

--- schema.sql
+++ schema.sql
@@ -1,10 +1,10 @@
-\c mythical_db
+\c mythical_db_test
 
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
 id SERIAL PRIMARY KEY,
 email VARCHAR UNIQUE NOT NULL,
-username VARCHAR UNIQUE NOT NULL,
+username VARCHAR NOT NULL,
 password_digest VARCHAR NOT NULL
 )
--- server.rb
+++ server.rb
@@ -8,6 +8,8 @@ require "bcrypt"
 		enable :sessions
 		set :method_override, true
 
+		@@db = PG.connect(dbname: "mythical_db_test")
+
 		get "/" do 
 			redirect "/signup"
 		end
@@ -15,6 +17,23 @@ require "bcrypt"
 		get "/signup" do 
 			erb :signup
 		end
+
+		post "/signup" do 
+			encrypted_password = BCrypt::Password.create(params[:login_password])
+
+			users = @@db.exec_params(<<-SQL, [params[:email], params[:username], encrypted_password])
+			  INSERT INTO users (email, username, password_digest) VALUES ($1, $2, $3) RETURNING id;
+			SQL
+
+			session["user_id"] = users.first["id"]
+
+			erb :signup_success
+
+		end
+
+
+
+		
 	end
 
 
