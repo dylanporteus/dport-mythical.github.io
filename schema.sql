c1ae185f0ca5292994a35a7d4b9f03c6639808e1
Dylan Porteus <dylanporteus@aol.com>
2016-01-21 16:33:50 -0500

    add topics and comments create tables to schema

--- schema.sql
+++ schema.sql
@@ -1,9 +1,25 @@
 \c mythical_db_test
 
-DROP TABLE IF EXISTS users;
+
 
 CREATE TABLE users (
 id SERIAL PRIMARY KEY,
 username VARCHAR UNIQUE NOT NULL,
 password_digest VARCHAR NOT NULL
 )
+
+CREATE TABLE topics(
+id SERIAL PRIMARY KEY,
+topic VARCHAR,
+user_id INTEGER REFERENCES users(id) 
+)
+
+CREATE TABLE comments(
+id SERIAL PRIMARY KEY,
+comment VARCHAR,
+user_id INTEGER REFERENCES users(id),
+topic_id INTEGER REFERENCES topics(id)
+)
+
+
+
